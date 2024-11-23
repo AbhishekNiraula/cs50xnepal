33d888c55571d40d075f153114bfcf7731d1cc26
Abhishek Niraulanpx install react-spinners <080bei004@ioepc.edu.np>
2024-11-23 20:37:45 +0545

    CS50xNepal

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prefer-const */
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
